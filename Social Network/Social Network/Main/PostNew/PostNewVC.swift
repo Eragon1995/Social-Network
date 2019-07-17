6b6501b9436113074545f951fae34d0feaeb48a7
huskyBo <eragon.xtb@gmail.com>
2019-07-17 15:44:31 +0700

    add

---
+++ Social Network/Social Network/Main/PostNew/PostNewVC.swift
@@ -0,0 +1,27 @@
+//
+//  PostNewVC.swift
+//  Social Network
+//
+//  Created by Husky on 7/17/19.
+//  Copyright © 2019 eragon. All rights reserved.
+//
+
+import UIKit
+
+class PostNewVC: BaseViewController {
+
+    @IBAction func touchShare(_ sender: Any) {
+//        let text = "This is the text...."
+//        let image = UIImage(named: "Product")
+//        let myWebsite = NSURL(string:"https://www.impl.vn")
+//        let shareAll = [text, image, myWebsite] as [Any]
+//        let activityViewController = UIActivityViewController(activityItems: shareAll, applicationActivities: nil)
+//        activityViewController.popoverPresentationController?.sourceView = self.view
+//        self.present(activityViewController, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
